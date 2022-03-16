a6f81aaf64f24351b23d842d86aac5ae294abd13
carolchep <carolchepsam@gmail.com>
2022-03-16 12:52:36 +0300

    Listing one product(gas)

---
+++ components/GasList.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE SWEETEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Leta Pizza makes sure you get your pizza in time and fresh.Enjoy with friends and  family and be on the lookout for our generous offers.
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
