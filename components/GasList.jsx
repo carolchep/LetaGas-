aedcc750f3c137ffc0174c76ed8681555f6f0a60
carolchep <carolchepsam@gmail.com>
2022-03-25 17:51:37 +0300

    tagline

--- components/GasList.jsx
+++ components/GasList.jsx
@@ -6,7 +6,7 @@ const GasList = ({ gasList }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>THE SWEETEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
-        Leta Pizza makes sure you get your pizza in time and fresh.Enjoy with friends and  family and be on the lookout for our generous offers.
+        Leta Gas takes off the heavy lifting of your gas ordering process.
       </p>
       <div className={styles.wrapper}>
         {gasList.map((pizza) => (
