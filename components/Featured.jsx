407881077a9d01d171d8720f2e628175e92366ae
carolchep <carolchepsam@gmail.com>
2022-04-08 16:52:08 +0300

    ON HOVER LINKS

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,15 +1,27 @@
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
-import { useState } from "react";
-import Navbar from "./Navbar";
+import { useSelector } from "react-redux";
+
+import Link from "next/link";
 
 const Featured = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
 
 
   return (
-    <div className={styles.container}>
-      <h1>YOU ORDER WE DELIVER</h1>
-    </div>
+      <div className={styles.container}>
+<div className={styles.nav}>
+    <h2>LETAGAS.</h2>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</div>
+        <h1>YOU ORDER WE DELIVER</h1>
+
+
+      </div>
   );
 };
 
